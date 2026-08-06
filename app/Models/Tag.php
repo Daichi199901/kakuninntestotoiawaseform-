9397367af28ae47b94af3c79d198ee365635589f
Daichi199901 <daichi199901011@gmail.com>
2026-08-06 10:27:55 +0900

    feat: モデル作成とリレーション定義 #2

---
+++ app/Models/Tag.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tag extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+    ];
+
+    /**
+     *このタグが紐付けられている全てのお問い合わせを取得 
+     */
+    public function contacts()
+    {
+        return $this->belongsToMany(Contact::class)->withTimestamps();
+    }      
+}
